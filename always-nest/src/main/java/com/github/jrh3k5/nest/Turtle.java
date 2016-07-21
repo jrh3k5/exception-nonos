5a54f32c0720262f47efd506c1ad899d52ff626c
Joshua Hyde <jrh3k5@gmail.com>
2016-07-21 13:50:51 -0600

    Refactor always-nest test to pass, while still demonstrating the issue

--- always-nest/src/main/java/com/github/jrh3k5/nest/Turtle.java
+++ always-nest/src/main/java/com/github/jrh3k5/nest/Turtle.java
@@ -1,5 +1,25 @@
 package com.github.jrh3k5.nest;
 
+/*-
+ * #%L
+ * Nesting Exceptions
+ * %%
+ * Copyright (C) 2016 jrh3k5
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.util.Objects;
 
 import org.slf4j.Logger;
@@ -13,7 +33,7 @@ import org.slf4j.LoggerFactory;
 
 public class Turtle implements Mover {
     private static final Logger LOGGER = LoggerFactory.getLogger(Turtle.class);
-    private final Turtle descendent;
+    private final Mover descendent;
 
     /**
      * Create a turtle that is the prime mover.
@@ -26,11 +46,11 @@ public class Turtle implements Mover {
      * Create a turtle that has a descendent turtle.
      * 
      * @param descendent
-     *            The {@link Turtle} descendent of this turtle.
+     *            The {@link Mover} descendent of this turtle.
      * @throws NullPointerException
      *             If the given descendent is {@code null}.
      */
-    public Turtle(Turtle descendent) {
+    public Turtle(Mover descendent) {
         this.descendent = Objects.requireNonNull(descendent, "Descendent cannot be null.");
     }
 
--- always-nest/src/test/java/com/github/jrh3k5/nest/UniverseTest.java
+++ always-nest/src/test/java/com/github/jrh3k5/nest/UniverseTest.java
@@ -1,5 +1,26 @@
 package com.github.jrh3k5.nest;
 
+/*-
+ * #%L
+ * Nesting Exceptions
+ * %%
+ * Copyright (C) 2016 jrh3k5
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
@@ -8,6 +29,8 @@ import java.util.List;
 
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Tests for the universe.
@@ -16,6 +39,8 @@ import org.junit.Test;
  */
 
 public class UniverseTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UniverseTest.class);
+
     /**
      * Test what happens when you don't nest your exceptions as causes.
      * 
@@ -41,7 +66,15 @@ public class UniverseTest {
             turtles.add(turtle);
         }
 
+        MoveException caught = null;
+        try {
         turtles.get(turtles.size() - 1).move(universe);
+        } catch (MoveException e) {
+            caught = e;
+        }
+
+        assertThat(caught).isNotNull();
+        LOGGER.info("An expected exception was caught during the test.", caught);
     }
 
 }
