5b065579f78bf1e1d87801722601bd9ac0298d61
Joshua Hyde <jrh3k5@gmail.com>
2016-07-21 09:22:24 -0600

    Automatically decorate source files with ASL 2.0 license

--- log-and-rethrow/src/main/java/com/github/jrh3k5/rethrow/DaughterTheftDao.java
+++ log-and-rethrow/src/main/java/com/github/jrh3k5/rethrow/DaughterTheftDao.java
@@ -1,5 +1,25 @@
 package com.github.jrh3k5.rethrow;
 
+/*-
+ * #%L
+ * Log and Rethrow
+ * %%
+ * Copyright (C) 2016 jrh3k5
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.sql.SQLException;
 import java.util.Objects;
 
